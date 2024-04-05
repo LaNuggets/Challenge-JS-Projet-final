6c59a3b4735c6a91c505a354c9f1dba4735abf9b
LaNuggets <150001502+LaNuggets@users.noreply.github.com>
2024-04-05 11:21:00 +0200

    RemoveJsFromHTML

---
+++ script.js
@@ -0,0 +1,15 @@
+const board = document.querySelector('.board');
+for (let row = 0; row < 16; row++) {
+    for (let col = 0; col < 16; col++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        const isEvenRow = row % 2 === 0;
+        const isEvenCol = col % 2 === 0;
+        if ((isEvenRow && !isEvenCol) || (!isEvenRow && isEvenCol)) {
+            square.classList.add('light-square');
+        } else {
+            square.classList.add('dark-square');
+        }
+        board.appendChild(square);
+    }
+}
