b7c1aae7b18cd966e7d152eb9551e93b2f1ea12e
LaNuggets <150001502+LaNuggets@users.noreply.github.com>
2024-04-12 08:24:37 +0200

    testCanvas

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ for (let row = 0; row < 16; row++) {
         board.appendChild(square);
     }
 }
+
+function roundRect(x, y, w, h, radius)
+{
+  var canvas = document.getElementById("canvas6");
+  var context = canvas.getContext("2d");
+  var r = x + w;
+  var b = y + h;
+  context.beginPath();
+  context.strokeStyle="purple";
+  context.lineWidth="4";
+  context.moveTo(x+radius, y);
+  context.lineTo(r-radius, y);
+  context.quadraticCurveTo(r, y, r, y+radius);
+  context.lineTo(r, y+h-radius);
+  context.quadraticCurveTo(r, b, r-radius, b);
+  context.lineTo(x+radius, b);
+  context.quadraticCurveTo(x, b, x, b-radius);
+  context.lineTo(x, y+radius);
+  context.quadraticCurveTo(x, y, x+radius, y);
+  context.stroke();
+  context.fillStyle = "purple";
+  context.fill();
+}
