c820a495a52499929cab81cbfeaa7635f550571a
LaNuggets <150001502+LaNuggets@users.noreply.github.com>
2024-05-02 10:37:04 +0200

    all

--- Scripts/multiFruit.js
+++ Scripts/multiFruit.js
@@ -6,16 +6,15 @@ const ctx = canvas.getContext("2d");
 let cellSize = 43.75;
 
 const foodImg =new Image();
-foodImg.src = './Images/Pomme.png'
-
-const backgroundImage = new Image();
-backgroundImage.src = './Images/background.jpg'
+foodImg.src = '../Images/Pomme.png'
 
 const eatApple = new Audio();
-eatApple.src = './Sound/mangePomme.mp3'
+eatApple.src = '../Sound/mangePomme.mp3'
 
 const gameOver = new Audio();
-gameOver.src = './Sound/gameOver.mp3'
+gameOver.src = '../Sound/gameOver.mp3'
+
+let snakeImg = document.getElementById('snakeD'); // Initialisation de snakeImg
 
 
 let snake =[];
@@ -59,22 +58,21 @@ const checkCollision= (head, arr)=>{
 
 
 const drawGame=()=> {
+  const bodySnakeImg = document.getElementById('bodySnakeImg');
   ctx.clearRect(0,0,700,700);
-
-//   ctx.drawImage(backgroundImage,0,0); 
-
 foodXCoordinates.forEach((x, index) => {
     const y = foodYCoordinates[index];
     ctx.drawImage(foodImg, x, y);
 });
 
 
-  for(let i=0;i<snake.length;i++){
-    ctx.fillStyle = (i==0) ? "purple" : "red";
-    ctx.fillRect(snake[i].x,snake[i].y, cellSize, cellSize);
-    ctx.strokeStyle = "black";
-    ctx.fillRect(snake[i].x,snake[i].y, cellSize, cellSize);
+for (let i = 0; i < snake.length; i++) {
+  if (i === 0) {
+    ctx.drawImage(snakeImg, snake[i].x, snake[i].y, cellSize, cellSize);
+  } else {
+    ctx.drawImage(bodySnakeImg, snake[i].x, snake[i].y, cellSize, cellSize);
   }
+}
 
       let snakeX = snake[0].x;
       let snakeY = snake[0].y;
@@ -119,30 +117,39 @@ document.addEventListener("keydown", function (event) {
   switch (event.keyCode) {
     case 37: // flèche gauche
       if (speedX !== 1) {
+        snakeImg = document.getElementById('snakeG');
         speedX = -1;
         speedY = 0;
       }
       break;
     case 38: // flèche haut
-      
       if (speedY !== 1) {
+        snakeImg = document.getElementById('snakeH');
         speedX = 0;
         speedY = -1;
       }
       break;
     case 39: // flèche droite
-      // Change la direction vers la droite, à condition que le serpent ne soit pas déjà dirigé vers la gauche
       if (speedX !== -1) {
+        snakeImg = document.getElementById('snakeD');
         speedX = 1;
         speedY = 0;
       }
       break;
     case 40: // flèche bas
-      // Change la direction vers le bas, à condition que le serpent ne soit pas déjà dirigé vers le haut
       if (speedY !== -1) {
+        snakeImg = document.getElementById('snakeB');
         speedX = 0;
         speedY = 1;
       }
       break;
   }
+
+  const replayButton = document.getElementById("replayButton");
+
+  replayButton.addEventListener("click", function () {
+      // Réinitialisation du jeu
+      clearInterval(game); // Arrête le jeu actuel
+      initGame(); // Réinitialise le jeu
+  });
 });
